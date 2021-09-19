190abeccf61184c7d50d29823f174ba95683292f
Sadekujjaman Saju <sadekujjamansaju474@gmail.com>
2021-09-19 22:35:17 +0600

    Create booklist component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,12 @@
+
+export default function BookList(){
+
+
+    return(
+        <div>
+            <ul id='book-list'>
+                <li>Book Name</li>
+            </ul>
+        </div>
+    )
+}
